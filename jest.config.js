ba4e7454077707bbcc9029e7eb2f32c6bcb78bd8
sulai001 <sulaiirf2930@gmail.com>
2025-07-20 14:04:51 +0530

    sadfgh

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+};
---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <a href="https://reactjs.org">Learn React</a>
+    </div>
+  );
+}
+
+export default App;
